e3b2b6d940210f61c277f49be682c535b707164f
bbom <benjamin.bomberg@stroeer-dialog.de>
2023-05-04 10:09:57 +0200

    change form layout

--- ext_localconf.php
+++ ext_localconf.php
@@ -12,5 +12,21 @@ $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['teaser'] = 'EXT:jlc_template/Conf
  */
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:jlc_template/Configuration/TsConfig/Page/jlc_template.tsconfig">');
 
+call_user_func(function () {
+    if (TYPO3_MODE === 'BE') {
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup(
+            trim('
+                module.tx_form {
+                    settings {
+                        yamlConfigurations {
+                            100 = EXT:jlc_template/Configuration/Form/CustomFormSetup.yaml
+                            101 = EXT:jlc_template/Configuration/Form/CustomBaseSetup.yaml
+                        }
+                    }
+                }
+            ')
+        );
+    }
 
+});
 $GLOBALS['TYPO3_CONF_VARS']['FE']['hidePagesIfNotTranslatedByDefault'] = 1;
